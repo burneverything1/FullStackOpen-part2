b5aa36b6801591a53e000dd518c0d8d8181ba600
burneverything1 <63659097+burneverything1@users.noreply.github.com>
2021-04-14 19:32:28 -0400

    Part 2.13

--- src/components/ShowCountries.js
+++ src/components/ShowCountries.js
@@ -1,31 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const SingleCountry = ({ country }) => {
+    return (
+        <>
+            <h1>{country.name}</h1>
+            <p>capital {country.capital}</p>
+            <p>population {country.population}</p>
+            <h2>languages</h2>
+            <ul>
+                {country.languages.map(language => <li key={language.iso639_1}>{language.name}</li>)}
+            </ul>
+            <img src={country.flag} alt={country.name} height="200"/>
+        </>
+    )
+}
 
 const ShowCountries = ({ countries, displayMode }) => {
+    const [ singleCountry, setSingleCountry ] = useState({})
 
+    let display;
+
+    const displaySingleCountry = (country) => {
+        setSingleCountry(country.name)
+    }
 
     if (displayMode === 3){
-        return (
+        display =
             <ul>
-                {countries.map(country => <li key={country.alpha2Code}>{country.name}</li>)}
+                {countries.map(country => <li key={country.alpha2Code}>{country.name}<button onClick={() => displaySingleCountry(country)}>show</button></li>)}
             </ul>
-        )
     } else if (displayMode === 2) {
-        return <p>Too many matches, specify another filter</p>
+        display = <p>Too many matches, specify another filter</p>
     } else {
         let country = countries[0]
-        return (
-            <>
-                <h1>{country.name}</h1>
-                <p>capital {country.capital}</p>
-                <p>population {country.population}</p>
-                <h2>languages</h2>
-                <ul>
-                    {country.languages.map(language => <li key={language.iso639_1}>{language.name}</li>)}
-                </ul>
-                <img src={country.flag} alt={country.name} width="200" height="200"/>
-            </>
-        )
+        display = <SingleCountry country={country} />
     }
+
+    return display
 }
 
 export default ShowCountries
